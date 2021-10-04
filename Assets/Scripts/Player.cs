5fef7bfe2b1b88d455c9fe8c0367d6898c22b31c
m.bespalov <max.bespalov23@gmail.com>
2021-10-04 11:47:06 +0300

    levels added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,26 +9,34 @@ public class Player : MonoBehaviour
     [SerializeField] float runSpeed = 4f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(1f, 10f);
 
     Rigidbody2D myRigidbody;
     Animator myAnimator;
-    Collider2D myCollider2D;
+    BoxCollider2D myBodyCollider2D;
+    CapsuleCollider2D myFeetCollider2D;
     float gravityScaleAtStart;
 
+    bool isAlive;
     void Start()
     {
+        isAlive = true;
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider2D = GetComponent<Collider2D>();
+        myBodyCollider2D = GetComponent<BoxCollider2D>();
+        myFeetCollider2D = GetComponent<CapsuleCollider2D>();
         gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
     void Update()
     {
+        if (!isAlive) return;
+
         Run();
         Jump();
         ClimbLadder();
         FlipSprite();
+        Die();
     }
 
     public void Run()
@@ -44,7 +52,8 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        if (CrossPlatformInputManager.GetButtonDown("Jump") && myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if (CrossPlatformInputManager.GetButtonDown("Jump")
+            && myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidbody.velocity += jumpVelocityToAdd;
@@ -53,7 +62,7 @@ public class Player : MonoBehaviour
 
     private void ClimbLadder()
     {
-        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladders")))
+        if (!myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladders")))
         {
             myAnimator.SetBool("Climbing", false);
             myRigidbody.gravityScale = gravityScaleAtStart;
@@ -76,6 +85,18 @@ public class Player : MonoBehaviour
         if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), transform.localScale.y);
+        }
+    }
+
+    private void Die()
+    {
+        if (myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            myFeetCollider2D.size = new Vector2(myFeetCollider2D.size.x, myFeetCollider2D.size.y * 0.7f);
+            //transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - 1f);
         }
     }
 }
